5119cd714605560f2ec3dad46fcdeb710c492fd5
Pavel Skaldin <pskaldin@zinc.it>
2021-04-05 12:42:18 -0700

    Modernize Package.swift
    * Explicitly declare swift-tools-version: 5.3
    * Include iOS, tvOs and macOS platforms
    * Include swift tests

--- Package.swift
+++ Package.swift
@@ -1,9 +1,30 @@
+// swift-tools-version:5.3
+// The swift-tools-version declares the minimum version of Swift required to build this package.
 import PackageDescription
 
 let package = Package(
-    name: "DateToolsSwift",
-    targets: [
-        Target(name: "DateToolsSwift")
-    ]
-)
-package.exclude = ["DateTools", "Examples", "Tests", "DateToolsSwift/Examples"]
+     name: "DateToolsSwift",
+     defaultLocalization: "en",
+     platforms: [
+         .iOS(.v8),
+         .tvOS(.v9),
+         .macOS(.v10_10),
+     ],
+     products: [
+         .library(
+             name: "DateToolsSwift", targets: ["DateToolsSwift"]),
+     ],
+     dependencies: [
+     ],
+     targets: [
+         .target(name: "DateToolsSwift",
+                 path: "DateToolsSwift/DateTools",
+                 resources: [.copy("DateTools.bundle")],
+                 swiftSettings: nil),
+         .testTarget(
+             name: "DateToolsSwiftTests",
+             dependencies: ["DateToolsSwift"],
+             path: "DateToolsSwift/Tests/DateToolsTests",
+             exclude: []),
+     ]
+ )
