073480226e6bf11bdc9d4064699e031e896c6785
Pavel Skaldin <pskaldin@zinc.it>
2021-04-05 17:00:59 -0700

    Clean up Package.swift

--- Package.swift
+++ Package.swift
@@ -4,7 +4,6 @@ import PackageDescription
 
 let package = Package(
     name: "DateToolsSwift",
-    defaultLocalization: "en",
     platforms: [
         .iOS(.v9),
         .tvOS(.v9),
@@ -12,20 +11,17 @@ let package = Package(
         .watchOS(.v2)
     ],
     products: [
-        .library(
-            name: "DateToolsSwift", targets: ["DateToolsSwift"]),
+        .library(name: "DateToolsSwift", targets: ["DateToolsSwift"])
     ],
     dependencies: [
     ],
     targets: [
         .target(name: "DateToolsSwift",
                 path: "DateToolsSwift/DateTools",
-                resources: [.copy("DateTools.bundle")],
-                swiftSettings: nil),
+                resources: [.copy("DateTools.bundle")]),
         .testTarget(
             name: "DateToolsSwiftTests",
             dependencies: ["DateToolsSwift"],
-            path: "DateToolsSwift/Tests/DateToolsTests",
-            exclude: ["DateToolsTests"]),
+            path: "DateToolsSwift/Tests/DateToolsTests/DateToolsTestsTests")
     ]
 )
