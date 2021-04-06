1221204da24816dfe851635f06b40eafe1e93cc6
Pavel Skaldin <pskaldin@zinc.it>
2021-04-05 17:34:15 -0700

    Make public DateToolsLocalizedString

--- DateToolsSwift/DateTools/Date+LocalizedString.swift
+++ DateToolsSwift/DateTools/Date+LocalizedString.swift
@@ -1,6 +1,6 @@
 import Foundation
 
-func DateToolsLocalizedString(_ key: String, tableName: String? = "DateTools", bundle: Bundle = Bundle.dateToolsBundle, value: String = "", comment: String = "") -> String {
+public func DateToolsLocalizedString(_ key: String, tableName: String? = "DateTools", bundle: Bundle = Bundle.dateToolsBundle, value: String = "", comment: String = "") -> String {
     #if os(Linux)
     // NSLocalizedString() is not available yet, see: https://github.com/apple/swift-corelibs-foundation/blob/16f83ddcd311b768e30a93637af161676b0a5f2f/Foundation/NSData.swift
     // However, a seemingly-equivalent method from NSBundle is:
